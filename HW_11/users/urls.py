6610b07286461ffead563611d7ef2f1c3e9e178d
Dmitry Shulga <Shulga.dmitry.16.1992@yandex.by>
2025-05-12 21:32:07 +0300

    Iteration 12

--- HW_11/users/urls.py
+++ HW_11/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
+from . import views
 # from .views import logout_view
 
 
 app_name = "users"
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
     # path('logout/', logout_view, name='logout'),
 ]
